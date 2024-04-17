1c324470729ac4fa10eae8ebca38c642953218c9
Ерарыс <arsikabylkasymovic@gmail.com>
2024-04-17 11:01:01 +0500

    add task code

--- task_1.py
+++ task_1.py
@@ -1,1 +1,30 @@
-print("Hello world")
+from rx import of
+from rx.operators import concat
+import rx.operators as op
+
+source = of("Task_1", "Task_2", "Task_3", "Task_4")
+
+
+while True:
+    action = input("Action: ")
+
+    if action == "add":
+        added_item = input("Added item: ")
+        source = source.pipe(concat(of(added_item)))
+    elif action == "del":
+        ob = input("Del item: ")
+        source = source.pipe(op.filter(lambda i: i != ob))
+    else:
+        break
+
+    source.subscribe(
+        on_next=lambda i: print("Received {0}".format(i)),
+        on_error=lambda e: print("Error Occurred: {0}".format(e)),
+        on_completed=lambda: print("Done!")
+    )
+
+source.subscribe(
+    on_next=lambda i: print("Received {0}".format(i)),
+    on_error=lambda e: print("Error Occurred: {0}".format(e)),
+    on_completed=lambda: print("Done!")
+)
